7927ac9e7a0c550aa5edc9c56bb385c4835f52dd
isabellaronnanderson-png <isabellaronnanderson@gmail.com>
2026-08-10 14:36:32 +0100

    Delete dispatch directory

--- dispatch/src/components/CategoryStamp.jsx
+++
@@ -1,6 +0,0 @@
-import { categoryMeta } from '../lib/categories';
-
-export default function CategoryStamp({ category }) {
-  const meta = categoryMeta(category);
-  return <span className={`stamp ${meta.stampClass}`}>{meta.label}</span>;
-}
